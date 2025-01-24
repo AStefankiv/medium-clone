306f676e58ce5e08bcb62d5ced94bd878f157984
AStefankiv <stefankif35@gmail.com>
2025-01-23 22:00:54 -0700

    Add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAgntNyJbjmCV3zjThideKoBDDHa8CfCE",
+  authDomain: "medium-42770.firebaseapp.com",
+  projectId: "medium-42770",
+  storageBucket: "medium-42770.firebasestorage.app",
+  messagingSenderId: "537432516476",
+  appId: "1:537432516476:web:5e04959137f2fff78326f9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,8 +1,45 @@
+// import { useParams, useNavigate } from 'react-router-dom';
+// import { useState } from 'react';
+// import { articles } from '../data/Articles';
+// import '../styles/Article.css';
+// import ArticleEditor from '../components/ArticleEditor';
+
+// const Article = () => {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+//   const [articleData, setArticleData] = useState(
+//     articles.find((article) => article.id === parseInt(id))
+//   );
+
+//   const handleSave = (updatedArticle) => {
+//     console.log('Updated article:', updatedArticle.content);
+//     setArticleData(updatedArticle);
+//     navigate(`/article/${id}`);
+//   }
+
+//   return (
+//     <div className="article">
+//       {articleData ? (
+//         <ArticleEditor article={articleData} onSave={handleSave} />
+//       ) : (
+//         <p>Article not found</p>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Article;
+
+
+
+
 import { useParams, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { articles } from '../data/Articles';
 import '../styles/Article.css';
 import ArticleEditor from '../components/ArticleEditor';
+import { collection, addDoc } from 'firebase/firestore';
+import db from '../firebase/firebase';
 
 const Article = () => {
   const { id } = useParams();
@@ -16,6 +53,8 @@ const Article = () => {
     setArticleData(updatedArticle);
     navigate(`/article/${id}`);
   }
+
+
 
   return (
     <div className="article">
