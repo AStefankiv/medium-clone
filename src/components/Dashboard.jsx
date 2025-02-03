4cf5fab9e02b56750c1a26e12fa1f1a202972e06
AStefankiv <stefankif35@gmail.com>
2025-02-02 22:37:03 -0700

    Protected Route edit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,13 @@
-import { useAuthState } from 'react-firebase-hooks/auth';
+// import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase/firebase';
+import { useAuth } from '../context/AuthContext';
 
 const Dashboard = () => {
-  const [user, loading] = useAuthState(auth);
+  // const [user, loading] = useAuthState(auth);
+  const { user, loading } = useAuth();
 
   if (loading) return <p>Loading...</p>;
-  if (!user) return <p>No user logged in</p>;
+  // if (!user) return <p>No user logged in</p>;
 
   return (
     <div>
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
-import { useAuthState } from 'react-firebase-hooks/auth';
+// import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase/firebase';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-  const [user] = useAuthState(auth);
+  // const [user] = useAuthState(auth);
+  const { user } = useAuth();
+  console.log("ProtectedRoute -> user", user)
 
   if (!user) {
     return <Navigate to="/signin" />;
