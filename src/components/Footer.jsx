a21b7a045a7d6541abdd00fa6141710e153ef69b
AStefankiv <stefankif35@gmail.com>
2025-02-04 22:40:45 -0700

    Add links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,14 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
+      <div>
+        <a href="https://github.com/AStefankiv" target="_blank">Github</a>
+      </div>
       <div className="footer__center">
-        <p>Medium Clone - {new Date().getFullYear()}</p>
+        <p><strong>Medium Clone - {new Date().getFullYear()}</strong></p>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/andrii-stefankiv/" target="_blank">Linked In</a>
       </div>
     </footer>
   );
